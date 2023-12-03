9b7fdf26aef4a04c196c6095a48f74d6be4a2e39
FabianHerrera25 <57154986+FabianHerrera25@users.noreply.github.com>
2023-12-03 15:24:13 -0600

    Update login.dart
    integracion de fincionalidad con diseño

--- lib/login.dart
+++ lib/login.dart
@@ -1,22 +1,87 @@
+import 'package:flamingo/main.dart';
 import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 
-class LoginPage extends StatelessWidget {
+class AuthService {
+  static const String apiUrl = "http://flamingosoftapi.somee.com/api/Usuarios";
+  static String? _token;
+
+  static String? get token => _token;
+
+  Future<bool> login(String username, String password) async {
+    try {
+      final Uri uri = Uri.parse('$apiUrl/Login?email=$username&Pass=$password');
+      final response = await http.get(uri);
+
+      print('Response status code: ${response.statusCode}');
+      print('Response body: ${response.body}');
+
+      if (response.statusCode == 200) {
+        // Assuming the response is a JWT token
+        String token = response.body;
+        _token = token;
+        saveToken(_token!);
+        return true;
+      } else {
+        print(
+            'Login failed. Status code: ${response.statusCode}, Body: ${response.body}');
+        return false;
+      }
+    } catch (e) {
+      print('Error during login: $e');
+      return false;
+    }
+  }
+
+  Future<void> saveToken(String token) async {
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setString('token', token);
+  }
+
+  Future<String?> getToken() async {
+    if (_token != null) {
+      return _token;
+    }
+
+    final prefs = await SharedPreferences.getInstance();
+    _token = prefs.getString('token');
+    return _token;
+  }
+
+  Future<void> logout() async {
+    _token = null;
+    final prefs = await SharedPreferences.getInstance();
+    prefs.remove('token');
+  }
+}
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  AuthService _authService = AuthService();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
-        fit: StackFit.expand, // La imagen de fondo ocupa toda la pantalla
+        fit: StackFit.expand,
         children: [
-          // Fondo con imagen
           Container(
             decoration: BoxDecoration(
               image: DecorationImage(
                 image: AssetImage('assets/fondo.jpg'),
-                fit: BoxFit.fill, // La imagen de fondo ocupa toda la pantalla
+                fit: BoxFit.fill,
               ),
             ),
           ),
-          // Contenedor con elementos de la interfaz de usuario
           Center(
             child: SingleChildScrollView(
               child: Padding(
@@ -25,15 +90,14 @@ class LoginPage extends StatelessWidget {
                   mainAxisAlignment: MainAxisAlignment.center,
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: <Widget>[
-                    // Imagen encima de los elementos
                     Image.asset(
                       'assets/logo2.png',
-                      width: 150, // ajusta el ancho según sea necesario
-                      height: 150, // ajusta la altura según sea necesario
+                      width: 150,
+                      height: 150,
                     ),
-                    SizedBox(height: 24), // Separación entre la imagen y el TextField
-                    // Input para Correo Electrónico
+                    SizedBox(height: 24),
                     TextField(
+                      controller: _usernameController,
                       decoration: InputDecoration(
                         labelText: 'Ingrese su correo electrónico',
                         fillColor: Colors.white,
@@ -41,9 +105,9 @@ class LoginPage extends StatelessWidget {
                         contentPadding: EdgeInsets.symmetric(horizontal: 16.0),
                       ),
                     ),
-                    SizedBox(height: 24), // Separación entre TextField
-                    // Input para Contraseña
+                    SizedBox(height: 24),
                     TextField(
+                      controller: _passwordController,
                       decoration: InputDecoration(
                         labelText: 'Ingrese su contraseña',
                         fillColor: Colors.white,
@@ -52,11 +116,23 @@ class LoginPage extends StatelessWidget {
                       ),
                       obscureText: true,
                     ),
-                    SizedBox(height: 24), // Separación entre TextField y el botón
-                    // Botón para Iniciar Sesión con esquinas redondeadas
+                    SizedBox(height: 24),
                     ElevatedButton(
-                      onPressed: () {
-                        // Lógica para iniciar sesión aquí
+                      onPressed: () async {
+                        String username = _usernameController.text.trim();
+                        String password = _passwordController.text.trim();
+
+                        bool success = await _authService.login(username, password);
+
+                        if (success) {
+                          Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(builder: (context) => MyHomePage()),
+                          );
+                          print('Login successful');
+                        } else {
+                          print('Login failed');
+                        }
                       },
                       style: ElevatedButton.styleFrom(
                         primary: Colors.blue,
